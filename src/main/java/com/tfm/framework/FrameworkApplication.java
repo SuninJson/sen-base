8b2bd8b0e0fe8c0f7ef03726d574c2ae5a4d5272
huangsen <sen.huang@iquantex.com>
2018-12-20 16:36:58 +0800

    数据库初步搭建完成

--- src/main/java/com/tfm/framework/FrameworkApplication.java
+++ src/main/java/com/tfm/framework/FrameworkApplication.java
@@ -1,12 +1,18 @@
 package com.tfm.framework;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Evan Huang
+ * @date 2018-12-20
+ */
 @SpringBootApplication
+@MapperScan("com.tfm.framework.dao.mapper")
 public class FrameworkApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FrameworkApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FrameworkApplication.class, args);
+    }
 }
---
+++ src/main/java/com/tfm/framework/dao/BaseDao.java
@@ -0,0 +1,8 @@
+package com.tfm.framework.dao;
+
+/**
+ * @author Evan Huang
+ * @date 2018/12/20
+ */
+public abstract class BaseDao {
+}
